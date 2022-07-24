01c824c2c223d8b8884a6581735bfed3d76ac72e
EnzoSives <73887784+EnzoSives@users.noreply.github.com>
2022-07-24 14:39:01 -0300

    Cambios en clase veterinaria

--- Veterinaria/veterinaria.ts
+++ Veterinaria/veterinaria.ts
@@ -1,42 +1,67 @@
+import * as ReadlineSync from "readline-sync";
+
 class Veterinaria{
     private id : number;
     private nombre : string;
     private direccion : string;
     private telefono : number;
-    private clientes : Cliente[];
-    private pacientes : Pacientes[];
+    //private clientes : Cliente[];
+    //private pacientes : Pacientes[];
 
-    constructor(id : number, nombre : string, direccion : string, telefono : number, clientes : Cliente[], pacientes : Pacientes[]) {
+    constructor(id : number, nombre : string, direccion : string, telefono : number,/** clientes : Cliente[], pacientes : Pacientes[]**/) {
         this.id = id;
         this.nombre = nombre;   
         this.direccion = direccion;
         this.telefono = telefono;
-        this.clientes = clientes;
-        this.pacientes = pacientes;
+       // this.clientes = clientes;
+        //this.pacientes = pacientes;
     }  
 
-    altaCliente(cliente:Cliente){}
+    /**altaCliente(cliente:Cliente){
+        this.clientes.push(cliente);
+    }**/
     
-    modificarCliente(cliente:Cliente){}
+    /**modificarCliente(cliente:Cliente){
+        for (let i = 0; i < this.clientes.length; i++) { 
+            this.clientes[i].nombre =  ReadlineSync.question('Nombre de Cliente');
+    }}**/
 
-    bajaCliente(cliente:Cliente){}
+    /**bajaCliente(cliente:Cliente){
+        this.clientes.splice(this.clientes.indexOf(cliente));
+    }**/
 
-    altaPaciente(paciente:Pacientes){}
+    /**altaPaciente(paciente:Pacientes){
+        this.pacientes.push(paciente);
+    }**/
 
-    modificarPaciente(paciente:Pacientes){}
+    //modificarPaciente(paciente:Pacientes){}
 
-    bajaPaciente(paciente:Pacientes){} 
+    /**bajaPaciente(paciente:Pacientes){
+        this.pacientes.splice(this.pacientes.indexOf(paciente);
+    }**/ 
 
-    setTelefono(telefono:number){}
+    setTelefono(telefono:number){
+        this.telefono = telefono;
+    }
 
-    setDireccion(direccion:string){}
+    setDireccion(direccion:string){
+        this.direccion = direccion;
+    }
 
-    getId(){}
+    getId(){
+        return this.id;
+    }
 
-    getTelefono(){}
+    getTelefono(){
+        return this.telefono;
+    }
 
-    getDireccion(){}
+    getDireccion(){
+        return  this.direccion;
+    }
 
-    getNombre(){}
+    getNombre(){
+        return this.nombre;
+    }
     
 }
