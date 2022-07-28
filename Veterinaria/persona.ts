5472f84227fb557ab0acf168c5e1b6a8be31caf7
EnzoSives <73887784+EnzoSives@users.noreply.github.com>
2022-07-28 20:13:55 -0300

    Cambios en clase persona

--- Veterinaria/persona.ts
+++ Veterinaria/persona.ts
@@ -1,9 +1,37 @@
 export abstract class Persona{
+    private nombre: string;
+    private direccion: string;
+    private telefono: number;
+    
     
     abstract construirMensaje(): string;
     
     saludar(nombre: string): void {
         console.log('Hola' , nombre);
     }
-    
+
+    getNombre(): string {   
+        return this.nombre;
+    }
+
+    getDireccion(): string { 
+        return this.direccion;
+    }
+
+    getTelefono(): number {
+        return this.telefono;
+    }
+
+    setNombre(nombre: string): void {
+        this.nombre = nombre;
+    }
+
+    setDireccion(direccion: string): void {
+        this.direccion = direccion;
+    }
+
+    setTelefono(telefono: number): void {
+        this.telefono = telefono;
+    }
+
 }
