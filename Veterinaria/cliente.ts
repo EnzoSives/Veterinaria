ab418eff330abdebe751b31ce59eefe1493468bc
EnzoSives <73887784+EnzoSives@users.noreply.github.com>
2022-08-29 18:33:50 -0300

    Se arreglaron los comentarios

--- Veterinaria/cliente.ts
+++ Veterinaria/cliente.ts
@@ -6,8 +6,8 @@ export class Cliente extends Persona implements IComunicarse {
    private cuil:number;
    private visita: number;
     
-    constructor (id: number, direccion: string, telefono: number,nombre: string) {
-        super(id,nombre, direccion, telefono);
+    constructor (id: number,direccion: string, telefono: number,nombre: string) {
+        super(nombre, direccion, telefono,id);
         this.cuil = this.cuil;
         this.visita = this.visita;
     }
@@ -19,19 +19,7 @@ export class Cliente extends Persona implements IComunicarse {
         return "Mensaje";
     }
 
-    public getId() {
-        return this.id;
-    }
-   
-    public setId(newId:number) {
-        this.id = newId;
-    }
-
-    public generarId(): number {
-        return  this.id = Math.floor((Math.random() * 100) + 1);
-      }
-
-      getEsVip() {
+    getEsVip() {
         if (this.visita >=4){
             console.log ("Es vip");
         } else {
